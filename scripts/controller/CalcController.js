cdc423340146623a653382cc39ce7a7229961088
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-01-18 16:15:57 -0300

    aulas 17 e 18

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -16,6 +16,7 @@ class CalcController {
     setInterval(()=>{
       this.setDisplayDateTime()
     }, 1000);
+    this.setLastNumberToDisplay();
   }
 
   addEventListenerAll(element, events, func) {
@@ -26,11 +27,24 @@ class CalcController {
   }
 
   calc() {
-    let last = this._operation.pop();
+    let last = '';
+    
+    if(this._operation.length > 3) {
+      last = this._operation.pop();
+    }
+
     let result = eval(this._operation.join(""));
 
-    this._operation = [result, last];
-    console.log(this._operation);
+    if(last == '%') {
+      result = result/100;
+      this._operation = [result];
+    } else {
+      this._operation = [result];
+
+      if(last) this._operation.push(last);
+    }
+    
+    this.setLastNumberToDisplay();
   }
 
   pushOperationValue(value) {
@@ -54,7 +68,17 @@ class CalcController {
   }
 
   setLastNumberToDisplay() {
-    
+    let lastNumber;
+
+    for(let i = this._operation.length - 1; i >= 0; i--) {
+      if(!this.isOperator(this._operation[i])) {
+        lastNumber = this._operation[i];
+        break;
+      }
+    }
+
+    if(!lastNumber) lastNumber = 0;
+    this.displayCalc = lastNumber;
   }
 
   addOperation(value) {
@@ -66,6 +90,7 @@ class CalcController {
         console.log(value);
       } else {
         this.pushOperationValue(value);
+        this.setLastNumberToDisplay();
       }
     } else {
       if(this.isOperator(value)) {
@@ -81,10 +106,13 @@ class CalcController {
 
   clearAll() {
     this._operation = [];
+    this.setLastNumberToDisplay();
+
   }
 
   clearEntry() {
     this._operation.pop();
+    this.setLastNumberToDisplay();
   }
 
   setError() {
@@ -115,10 +143,10 @@ class CalcController {
         this.addOperation("%");
         break;
       case 'igual':
-        this.clearAll();
+        this.calc();
         break; 
       case 'ponto':
-        this.clearAll();
+        
         break; 
       case '0':
       case '1':
